0ab3cd22580aab7a58d0118180291f01c5427f18
Abdalaa Ahmad <abdalaa.ahmad@gmail.com>
2022-07-22 23:04:26 +0300

    CRUD Department

---
+++ app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\DepartmentRequest;
+use App\Http\Resources\DepartmentRecource;
+use App\Models\Department;
+use Illuminate\Http\Request;
+
+class DepartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return DepartmentRecource::collection(Department::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(DepartmentRequest $request)
+    {
+        $department = Department::create([
+            'name' => $request->input('name')
+        ]);
+
+        return new DepartmentRecource($department);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Department $department)
+    {
+        return new DepartmentRecource($department);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(DepartmentRequest $request, Department $department)
+    {
+        $department->update([
+            'name' => $request->input('name')
+        ]);
+
+        return new DepartmentRecource($department);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Department $department)
+    {
+        $department->delete();
+        return response(null, 204);
+    }
+}
